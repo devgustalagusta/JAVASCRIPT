1638fb5fc20c12da13527ab05d85c845fc5a6480
gustavopesso <gustavopg8989@gmail.com>
2023-06-29 16:52:09 -0300

    exercicios de JAVASCRIPT

---
+++ aula012/exe013.js
@@ -0,0 +1,28 @@
+var now = new Date
+var diasem = now.getDay()
+
+switch(diasem) {
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2:
+        console.log('Terça-feira')
+        break
+    case 3:
+        console.log('Quarta-feira')
+        break
+    case 4:
+        console.log('Quinta-feira')
+        break
+    case 5:
+        console.log('Sexta-feira')
+        break
+    case 6:
+        console.log('Sábado')
+    default:
+        console.log(' dia invalido!')
+        break
+}
