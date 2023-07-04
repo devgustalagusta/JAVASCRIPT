08ca9a7d517addb1fe9d8b28274b18859aac3318
gustavopesso <gustavopg8989@gmail.com>
2023-07-04 17:16:39 -0300

    exercicio de javascript
    calculador de idade

--- aula12exe/exe015/script.js
+++ aula12exe/exe015/script.js
@@ -1,12 +1,49 @@
-function verificar (){
-    var data = new Date()
-    var ano = data.getFullYear()
+function verificar() {
+    var date = new Date()
+    var ano = date.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
     if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('[ERRO] verifique os dados e tente novamente!')
     } else {
-        var fsex = document.getElementsById('radsex')
+        var fsex =document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'um Homem'
+            if (idade>=0 && idade < 10) {
+                // crianca
+                img.setAttribute('src', 'crianca-menino.JPEG')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'jovem-menino.JPEG')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'homem.JPEG')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idoso.JPEG')
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'uma Mulher'
+            if (idade>=0 && idade < 10) {
+                // crianca
+                img.setAttribute('src', 'crianca-menina.JPEG')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'jovem-menina.JPEG')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'mulher.JPEG')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idosa.JPEG')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} de ${idade} anos.`
+        res.appendChild(img)
     }
-}   
+}
