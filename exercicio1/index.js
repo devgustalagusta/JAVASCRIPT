24f91436b89d52efe7c9b97d5286356f68715d80
gustavopesso <gustavopg8989@gmail.com>
2023-08-08 17:31:47 -0300

    exercicios

---
+++ exercicio1/index.js
@@ -0,0 +1,7 @@
+nome = prompt('Qual o seu nome:')
+snome = prompt('Qual o seu sobrenome:')
+estudo = prompt('O que você estuda?')
+ano = prompt('Em que ano você nasceu?')
+idade = ano-2023
+
+alert("Eu me chamo " + nome + " " + snome + ", estudo " + estudo + " e tenho " + idade + " anos!")
---
+++ exercicio2/index.js
@@ -0,0 +1,10 @@
+const numero1 = prompt('Digite um número:')
+const numero2 = prompt('DIgite outro número:')
+const numeroum = numero1 + '';
+const numerodois = numero2 + '';
+let soma = numeroum + numerodois
+let sub = numeroum - numerodois
+let mult = numeroum * numerodois
+let div = numeroum / numerodois
+
+alert("O valor da soma dos dois números deu: " + soma + " A subtração dos valores deu: " + sub + " A multiplicação dos valores deu: " + mult + " e a divisão dos valores deu: " + div)
