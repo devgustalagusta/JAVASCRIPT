31b27f31292b3a853495894f520eda1cb539cc8f
gustavopesso <gustavopg8989@gmail.com>
2023-08-13 12:52:50 -0300

    exercicio

--- exercicios OneBitCode/exe while/index.js
+++ exercicios OneBitCode/exe while/index.js
@@ -1,4 +1,16 @@
-let nome = prompt("Qual o seu nome:")
+const nome = prompt("Qual o seu nome:")
+let cidades = ""
+let contagem = "0"
 
 let visitouCidade = prompt("Você já visitou alguma cidade: (sim) (não)")
 
+while (visitouCidade === "sim" ) { 
+    let cidade = prompt("Qual cidade você visitou?")
+    cidades += " - " + cidade + "\n"
+    contagem ++
+    visitouCidade = prompt("Você visitou alguma outra cidade? (sim) (não)")
+}
+alert(
+    "O turista visitou " + contagem + " Cidades\n" +
+    cidades
+)
