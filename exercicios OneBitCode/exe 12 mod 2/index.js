947e7fb4a1480c490632de4623c42b806057a3ec
gustavopesso <gustavopg8989@gmail.com>
2023-09-26 21:34:25 -0300

    atividade

--- exercicios OneBitCode/exe 12 mod 2/index.js
+++ exercicios OneBitCode/exe 12 mod 2/index.js
@@ -32,10 +32,18 @@ addTechBtn.addEventListener('click', function(ev){
     const techNameInput = creatInput('techName-' + rowIndex,null,'techName')
 
     const expLabel = creatLabel('Experiencia: ')
-    const expRadio1 = creatInput('expRadio-' + rowIndex + '.1', '0-2 anos', 'techExp-' + rowIndex)
+    const id1 = 'expRadio-' + rowIndex + '.1'
+    const expRadio1 = creatInput(id1, '0-2 anos', 'techExp-' + rowIndex, 'radio')
+    const expLabel1 = creatLabel('0-2 anos', id1)
+    const id2 = 'expRadio-' + rowIndex + '.2'
+    const expRadio2 = creatInput(id2, '3-4 anos', 'techExp-' + rowIndex, 'radio')
+    const expLabel2 = creatLabel('3-4 anos', id1)
+    const id3 = 'expRadio-' + rowIndex + '.3'
+    const expRadio3 = creatInput(id3, '5+ anos', 'techExp-' + rowIndex, 'radio')
+    const expLabel3 = creatLabel('5+ anos', id3)
 
     newRow.append(
-        techNameLabel, techNameInput
+        techNameLabel, techNameInput, expLabel, expRadio1, expLabel1, expRadio2, expLabel2,  expRadio3, expLabel3
     )
     stackInputs.appendChild(newRow)
 })
