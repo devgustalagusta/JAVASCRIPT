92bbfcae5aff8528b8f7111df849b3e71b0f9984
gustavopesso <gustavopg8989@gmail.com>
2023-09-28 16:14:48 -0300

    atividade

---
+++ exercicios OneBitCode/estilos com javaScript/index.js
@@ -0,0 +1,18 @@
+function userLightThemer (){
+    document.body.style.color = '#212529'
+    document.body.style.backgroundColor = '#f1f5f9'
+}
+
+function userDarkThemer (){
+    document.body.style.color = '#f1f5f9' 
+    document.body.style.backgroundColor = '#212529'
+}
+// -toggle serve para alternar, atraves das classes-
+function switchThemer (){
+    document.body.classList.toggle('is-light')
+    document.body.classList.toggle('is-dark')
+}
+
+document.getElementById('lightBtn').addEventListener('click', userLightThemer)
+document.getElementById('darkBtn').addEventListener('click', userDarkThemer)
+document.getElementById('switchBtn').addEventListener('click',switchThemer)
