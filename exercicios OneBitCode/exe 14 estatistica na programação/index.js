1d046364d82f9a4393e62d93d672af9213469945
gustavopesso <gustavopg8989@gmail.com>
2023-10-19 17:41:41 -0300

    estudos

--- exercicios OneBitCode/exe 14 estatistica na programação/index.js
+++ exercicios OneBitCode/exe 14 estatistica na programação/index.js
@@ -1,12 +1,12 @@
 // Média aritmética simples
-function avarage(numbers){
+function average(numbers){
     const sum = numbers.reduce((accum, num) =>{
         return accum + num
     },0)
     return sum / numbers.length
 }
 
-console.log(avarage([2,6,3,7,4]))
+console.log(average([2,6,3,7,4]))
 // Média aritmética ponderada
 const weighttedAverage = (...entries) => {
     const sum = entries.reduce((accum,{number, weight}) => accum + (number * (weight ?? 1)),0)
@@ -18,3 +18,28 @@ console.log(`Média ponderada: ${weighttedAverage(
     {number: 7, },
     {number: 10, weight: 1},
 )}`)
+
+//mediana
+const median = (...numbers) => {
+    const orderedNumbers = [...numbers].sort((a,b) => a - b)
+    const middle = Math.floor(orderedNumbers.length / 2)
+    if (orderedNumbers.length % 2 !== 0) {
+        return orderedNumbers[middle]
+    }
+    const firstMedian = orderedNumbers[middle - 1]
+    const secondMedian = orderedNumbers[middle]
+    return average (firstMedian, secondMedian)
+}
+console.log(`Mediana: ${median(2,5,99,4,42,7)}`)
+console.log(`Mediana: ${median(15,14,8,7,3)}`)
+
+// Moda 
+const mode = (...numbers) =>{
+    const quantities = numbers.map(num => [
+        num,
+        numbers.filter(n => num === n).length
+    ])
+    quantities.sort((a,b)=> b[1] - a[1])
+    return quantities(0)(0)
+}
+console.log(`Moda: ${mode(1,1,99,99,99,99,99,99,99,5,4,9,7,5,4,46,4,8)}`)
