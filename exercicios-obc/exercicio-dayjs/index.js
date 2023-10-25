3df9dcf440d8c47ecfd94d371dca658ff794665d
gustavopesso <gustavopg8989@gmail.com>
2023-10-25 17:43:38 -0300

    estudos

---
+++ exercicios-obc/exercicio-dayjs/index.js
@@ -0,0 +1,10 @@
+const dayjs = require("dayjs")
+
+const dayjs = require(dayjs)
+dayjs().format()
+
+dayjs.extend(relativeTime)
+
+function birthday (date){
+    dayjs('2003-02-03').fromNow(true)
+}
